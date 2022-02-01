ef00587c9cb354854c3ebd193e2e0f6ba253d1be
乂Nat <83612604+Natx-C@users.noreply.github.com>
2022-02-02 01:51:32 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,7 @@ var zahirr = db.get("zahirr");
 	console.log('')  
 }
  
-var creator = "HADAMUHAMMAD"
+var creator = "nat"
 var secure = require('ssl-express-www');
 var cors = require('cors');
 var fetch = require('node-fetch');
@@ -175,7 +175,7 @@ Akhir Pesan Error
 
 router.use(favicon(__path + "/views/favicon.ico"));
 
-const listkey = ["HADAKEY", "HADAKT", "HADATZY", "BeliBang"];
+const listkey = ["apinat", "natapi"];
 
 router.post("/apikey", async (req, res, next) => {
   const key = req.query.key;
